8fad1b60e4c9904bc37e9f6a7ba265da50bbb818
abhinandh <abhinandh@gmail.com>
2009-12-02 00:53:09 +0530

    able to solve levels 1,2,3

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 //      main.cpp
 //
-//      Copyright 2009 Unknown <abhi@eeepc>
+//      Copyright 2009 Abhinandh <abhi@abhinandh.com>
 //
 //      This program is free software; you can redistribute it and/or modify
 //      it under the terms of the GNU General Public License as published by
@@ -22,20 +22,26 @@
 
 #include <iostream>
 #include <QList>
-#define EMPTY 99
+#define EMPTY 99    //input 99 in place of an empty block
 
 using std::cout;
 using std::cin;
 using std::endl;
 
+int filled;
 struct block
 {
         int val;
         QList<int> p;
 };
 
+void fillLists(block table[][9]);
 void calcPossibilities(block table[][9]);
-void eliminate3x3P(block& item,int row,int col);
+inline void eliminate3x3P(block table[][9],block& item,int row,int col);
+
+void calcValues(block table[][9]);
+template <typename T>
+QList<T> & operator-= ( QList<T> & first,const QList<T> & other );
 
 int main(int argc, char** argv)
 {
@@ -49,26 +55,63 @@ int main(int argc, char** argv)
                         cin>>table[i][j].val;
                 }
         }
+     fillLists(table);
+     //calcPossibilities(table);
 
-     calcPossibilities(table);
 
-        return 0;
+     while(filled<81)
+     {
+       calcPossibilities(table);
+       calcValues(table);
+       cout<<filled<<endl;
+       /*for(int i=0;i<9;i++)            //display possibility list
+                for(int j=0;j<9;j++)
+         { for (int k = 0; k < table[i][j].p.size(); ++k) {
+                         cout<<table[i][j].p[k]<<" ";
+                     }
+           cout<<endl;
+         }
+       cout<<"foo";*/
+     }
+
+     /*for(int i=0;i<9;i++)            //display possibility list
+                for(int j=0;j<9;j++)
+         { for (int k = 0; k < table[i][j].p.size(); ++k) {
+                         cout<<table[i][j].p[k]<<" ";
+                     }
+           cout<<endl;
+       }*/
+
+     cout<<endl;
+     for(int i=0;i<9;i++) {           //display values
+         for(int j=0;j<9;j++)
+             cout<<table[i][j].val<<"\t";
+         cout<<"\n";
+     }
+
+     return 0;
 }
+
+void fillLists(block table[][9])
+{
+    for(int i=0;i<9;i++)             //fill lists with values from 0-9
+            for(int j=0;j<9;j++)
+            {     if(table[i][j].val!=EMPTY)
+                  {++filled;continue;}
+                 for(int k=1;k<=9;k++)
+                    table[i][j].p+=k;
+            }
+}
+
 
 void calcPossibilities(block table[][9])
 {
-        for(int i=0;i<9;i++)             //fill lists with values from 0-9
-            for(int j=0;j<9;j++)
-            {     if(table[i][j].val==EMPTY)
-                            continue;
-                for(int k=0;k<=9;k++)
-                    table[i][j].p+=k;
-            }
+
         //eliminate items in same row from possibilities
         for(int i=0;i<9;i++)
                 for(int j=0;j<9;j++)
                 {
-                        if(table[i][j].val==EMPTY)
+                        if(table[i][j].val!=EMPTY)
                             continue;
                         for(int k=0;k<9;k++)
                                 if(table[i][j].p.contains(table[i][k].val))
@@ -78,7 +121,8 @@ void calcPossibilities(block table[][9])
         //eliminate items in same column from possibilities
         for(int i=0;i<9;i++)
                 for(int j=0;j<9;j++)
-                 {   if(table[i][j].val==EMPTY)
+                 {
+                    if(table[i][j].val!=EMPTY)
                             continue;
                     for(int k=0;k<9;k++)
                                 if(table[i][j].p.contains(table[k][j].val))
@@ -88,19 +132,94 @@ void calcPossibilities(block table[][9])
         for(int i=0;i<9;i++)
                 for(int j=0;j<9;j++)
             {
-             if(table[i][j].val==EMPTY)
+             if(table[i][j].val!=EMPTY)
                             continue;
-             eliminate3x3P(table[i][j],i,j);
+             eliminate3x3P(table,table[i][j],i,j);
          }
 
 }
 
-void eliminate3x3P(block& item,int row,int col)
+void eliminate3x3P(block table[][9],block& item,int row,int col)
 {
     int startRow,startCol;
     startRow=row-row%3;
-    //startCol=;
-    cout<<startRow<<startCol<<endl;
+    startCol=col-col%3;
+
+    for(int i=startRow; i!=startRow+3 ; i++)
+        for(int j=startCol;j!=startCol+3;j++)
+        {
+          if(item.p.contains(table[i][j].val))
+                 item.p.removeOne(table[i][j].val);
+         }
 }
+
+
+void calcValues(block table[][9])
+{
+      for(int i=0;i<9;i++)
+          for(int j=0;j<9;j++)
+          {
+            if(table[i][j].p.isEmpty())       //skip already filled blocks
+                continue;
+            QList<int> tmp=table[i][j].p;
+            if(tmp.size()==1) {
+                table[i][j].val=tmp[0];
+                table[i][j].p.clear();
+                calcPossibilities(table);
+                ++filled;
+                continue;
+            }
+            for(int k=0;k<9;k++)
+            {
+                if(k==i)
+                    continue;
+                tmp-=table[k][j].p;
+            }
+            if(tmp.size()==1) {
+                table[i][j].val=tmp[0];
+                table[i][j].p.clear();
+                calcPossibilities(table);
+                ++filled;
+                continue;
+            }
+            tmp=table[i][j].p; //col check
+            for(int k=0;k<9;k++)
+            {
+                if(k==j)
+                    continue;
+                tmp-=table[i][k].p;
+            }
+
+            if(tmp.size()==1) {
+                table[i][j].val=tmp[0];
+                table[i][j].p.clear();
+                calcPossibilities(table);
+                ++filled;
+                continue;
+            }
+
+            }
+
+ }
+
+template <typename T>
+QList<T> & operator-= (QList<T> & first,const QList<T> & second )
+{
+    /*cout<<"first";
+    for (int i = 0; i < first.size(); ++i)
+        cout<<first[i];
+    cout<<"second";
+    for (int i = 0; i < second.size(); ++i)
+        cout<<second[i];*/
+    for (int i = 0; i < second.size(); ++i) {
+        if(first.contains(second[i]))
+            first.removeOne(second[i]);
+    }
+
+    return first;
+}
+
+
+
 
 
